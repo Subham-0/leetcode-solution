45d25b8c43a3b601175efea58dda7b63009e6773
Subham Dash <subhamd2022@gift.edu.in>
2026-02-19 00:24:08 +0530

    Time: 0 ms (100%), Space: 45.3 MB (36.77%) - LeetHub

--- 3151-special-array-i/3151-special-array-i.java
+++ 3151-special-array-i/3151-special-array-i.java
@@ -1,12 +1,9 @@
 class Solution {
     public boolean isArraySpecial(int[] nums) {
-        int len = nums.length;
-        if (len == 1)
-            return true;
-        for (int i = 1; i < len; i++) {
-            if ((nums[i] % 2 == 0 && nums[i - 1] % 2 == 0)
-                    || (nums[i] % 2 == 1 && nums[i - 1] % 2 == 1)) {
-                        return false;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] % 2 == nums[i - 1] % 2) {
+                return false;
             }
         }
         return true;
