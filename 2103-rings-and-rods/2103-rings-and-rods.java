c350bb45a6803107042a00f3a83db530a0f56807
Subham Dash <subhamd2022@gift.edu.in>
2026-02-19 02:12:43 +0530

    Time: 1 ms (54.91%), Space: 43 MB (29.44%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.java
+++ 2103-rings-and-rods/2103-rings-and-rods.java
@@ -1,12 +1,13 @@
 class Solution {
     public int countPoints(String rings) {
-        Map<Character, String> map = new HashMap();
+        Map<Character, Set<Character>> map = new HashMap();
         for (int i = 0; i < rings.length(); i += 2) {
-            map.put(rings.charAt(i + 1), map.getOrDefault(rings.charAt(i + 1), "") + rings.charAt(i));
+            map.putIfAbsent(rings.charAt(i + 1), new HashSet<>());
+            map.get(rings.charAt(i + 1)).add(rings.charAt(i));
         }
         int c = 0;
-        for (String val : map.values()) {
-            if (val.contains("R") && val.contains("G") && val.contains("B")) {
+        for (Set<Character> set : map.values()) {
+            if (set.size() == 3) {
                 c++;
             }
         }
