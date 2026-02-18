a8a843269db9a4cb2907dc2c42916684e650dabc
Subham Dash <subhamd2022@gift.edu.in>
2026-02-18 10:59:05 +0530

    Time: 6 ms (92.78%), Space: 71.9 MB (72.76%) - LeetHub

---
+++ 0848-shifting-letters/0848-shifting-letters.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String shiftingLetters(String s, int[] shifts) {
+        char[] str = s.toCharArray();
+        int total = 0;
+        for (int i = shifts.length - 1; i >= 0; i--) {
+            total = (total + shifts[i]) % 26;
+            str[i] = (char) ((str[i] - 'a' + total) % 26 + 'a');
+
+        }
+        return new String(str);
+    }
+}
