9f67e0a93afb17f5cbb5a150d9c5c25f23e0a8f2
Subham Dash <subhamd2022@gift.edu.in>
2026-02-20 12:45:07 +0530

    Time: 1 ms (71%), Space: 44.6 MB (33.88%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int c =0;
+        for(int i =0;i<details.length;i++){
+           if(Integer.valueOf(details[i].substring(11,13)) > 60){
+            c++;
+           }
+        }
+        return c;
+    }
+}
