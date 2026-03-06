08b8ab1f4f40be2eadf84f226e044f01cabb2f4d
Subham Dash <subhamd2022@gift.edu.in>
2026-03-06 10:13:20 +0530

    Time: 1 ms (56.67%), Space: 45.1 MB (29.23%) - LeetHub

---
+++ 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countTestedDevices(int[] batteryPercentages) {
+        int len = batteryPercentages.length;
+        int count = 0;
+        for (int i = 0; i < len; i++) {
+            if (batteryPercentages[i] > 0) {
+                for (int j = i + 1; j < len; j++) {
+                    if (batteryPercentages[j] > 0) {
+                        batteryPercentages[j]--;
+                    }
+                }
+                count++;
+            }
+        }
+        return count;
+    }
+}
